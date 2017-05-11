37d67a2468853037f557c5a40720ef011285f291
Carson <rcarsonsmith@gmail.com>
2017-05-10 20:00:15 -0400

    Added factory, root classes and dapper package
    This push adds the baseline definitions of factory and root classes, as
    well as implementing the dapper package, which was chosen due to it's
    speed compared to the entity framework

---
+++ TreeGenerator/Models/Factory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TreeGenerator.Models
+{
+    public class Factory
+    {
+        int ID { get; set; }
+        int parent { get; set; }
+        string name { get; set; }
+        int childrenCount { get; set; }
+        int lowBound { get; set; }
+        int upBound { get; set; }
+
+
+
+    }
+}
---
+++ TreeGenerator/Models/root.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TreeGenerator.Models
+{
+    public class Root
+    {
+       int ID { get; set; }
+       string name { get; set; }
+       string children_count { get; set; }
+       int active { get; set; }
+
+    }
+
+
+}
